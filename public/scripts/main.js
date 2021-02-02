b6dad180b5ebcccbda34dafa550c2c5a2eb97d5c
Anson <ansonlee51326@gmail.com>
2021-02-03 02:17:11 +0800

    adding messenegr

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,21 @@
 (() => {
     console.log('fired')
+
+    // load the socket libery and make a connection
+    const socket = io();
+
+    const vm = new Vue({
+        data: {
+            messages: [],
+            nickname: "",
+            username: ""
+        },
+
+        created: function() {
+            console.log('its alive!');
+        },
+        methods: {
+
+        }
+    }).$mount("#app");
 })();
