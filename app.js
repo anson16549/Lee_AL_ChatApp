11365ca249c807fadbeb8ae7593356443e89686b
Anson <ansonlee51326@gmail.com>
2021-02-18 12:31:10 +0800

    updating

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5050;
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html")); //localhost:3000/index.html
@@ -27,6 +27,14 @@ messenger.attach(server);
 // socket is th individual connection - the caller
 messenger.on('connection', (socket) => {
     console.log(`a user connected: ${socket.id}`);
+
+    //send the connected user their assigned ID
+    socket.emit('connected', { sID: `${socket.id}`, message: 'new connection' });
+
+    socket.on('chatmessage', function(msg) {
+        console.log(msg);
+        messenger.emit('message', { id: socket.id, message: msg });
+    });
 
     socket.on('disconnect', () => {
         console.log('a user has disconnected')
