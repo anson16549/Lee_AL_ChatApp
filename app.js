b9750b52a812bf4af8114e95a96bbecfddff84d0
Anson <ansonlee51326@gmail.com>
2021-02-20 12:16:58 +0800

    updating

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-const path = require('path');
-
 const express = require('express');
-const { isObject } = require('util'); //
+const path = require('path');
 const messenger = require('socket.io')();
 
-let users = [];
-let index = 0;
-let messages = [];
+// let users = []; //
+
+// let messages = []; //
 
 
 
 const app = express();
 
-
-app.use(express.static("public"));
-
 const port = process.env.PORT || 5050;
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/index.html")); //localhost:3000/index.html
 });
 
 app.get("/chat", (req, res) => {
-    res.sendFile(path.join(__dirname, "chat.html")); //localhost:3000/index.html
+    res.sendFile(path.join(__dirname, "/chat.html")); //localhost:3000/index.html
 });
 
 const server = app.listen(port, () => {
@@ -36,34 +32,34 @@ messenger.attach(server);
 messenger.on('connection', (socket) => {
     console.log(`a user connected: ${socket.id}`);
 
-    socket.on('newuser', username => {
-        console.log(`${username} has arrived,`); //
-        socket.username = username; //
-        users.push(socket); //
+    // socket.on('newuser', username => {
+    //     console.log(`${username} has arrived,`); //
+    //     socket.username = username; //
+    //     users.push(socket); //
 
-        messenger.emit('userOnline', socket.username);
-    });
+    //     messenger.emit('userOnline', socket.username);
+    // });
 
-    socket.on('msg', msg => {
-        let message = {
-            index: index,
-            username: socket.username,
-            msg: msg
-        }
-        messages.push(message);
-        messenger.emit('msg', message);
+    //     // socket.on('msg', msg => {
+    //     //     let message = {
+    //     //         index: index,
+    //     //         username: socket.username,
+    //     //         msg: msg
+    //     //     }
+    //     //     messages.push(message);
+    //     //     messenger.emit('msg', message);
 
-        index++;
-    });
+    //     //     index++;
+    // });
 
 
     //send the connected user their assigned ID
     socket.emit('connected', { sID: `${socket.id}`, message: 'new connection' });
 
-    socket.emit('loggedIn', {
-        users: users.map(s => s.username),
-        messages: messages
-    });
+    // socket.emit('loggedIn', {
+    //     users: users.map(s => s.username),
+    //     messages: messages
+    // });
 
     socket.on('chatMessage', function(msg) {
         console.log(msg);
@@ -74,9 +70,9 @@ messenger.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        //console.log('a user has disconnected')
-        console.log(`${socket.id} has left,`); //
-        messenger.emit("userLeft", socket.username);
-        users.splice(users.indexOf(socket), 1);
+        console.log('a user has disconnected')
+            // console.log(`${socket.id} has left,`); //
+            // messenger.emit("userLeft", socket.username);
+            // users.splice(users.indexOf(socket), 1);
     }); //
 });
